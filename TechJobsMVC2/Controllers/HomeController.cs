877fff9a474355a13735ec85a31a4901be2ceab1
Sam <swizrc@mst.edu>
2017-11-12 22:12:27 -0600

    Initialize

--- TechJobsMVC2/Controllers/HomeController.cs
+++ TechJobsMVC2/Controllers/HomeController.cs
@@ -1,37 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using TechJobsMVC2.Models;
 
-namespace TechJobsMVC2.Controllers
+namespace TechJobs.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
+            Dictionary<string, string> actionChoices = new Dictionary<string, string>();
+            actionChoices.Add("search", "Search");
+            actionChoices.Add("list", "List");
+            
+            ViewBag.actions = actionChoices;
 
             return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ TechJobsMVC2/Controllers/SearchController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TechJobs.Models;
+
+namespace TechJobs.Controllers
+{
+    public class SearchController : Controller
+    {
+        static private List<Dictionary<string, string>> Jobs = new List<Dictionary<string, string>>();
+        static private string search;
+
+        public IActionResult Index()
+        {
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Search";
+            ViewBag.jobs = Jobs;
+            ViewBag.search = search;
+            return View();
+        }
+
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            if (searchTerm != null)
+            {
+                if (searchType == "all")
+                {
+                    Jobs = JobData.FindByValue(searchTerm);
+                }
+                else
+                {
+                    Jobs = JobData.FindByColumnAndValue(searchType,searchTerm);
+                }
+            }
+            search = searchTerm;
+            return Redirect("/Search");
+        }
+        // TODO #1 - Create a Results action method to process 
+        // search request and display results
+
+    }
+}
